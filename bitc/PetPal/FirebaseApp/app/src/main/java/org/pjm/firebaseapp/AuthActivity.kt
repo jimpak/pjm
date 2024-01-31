fff24679e5447b6f385f3f45e0098373d77d76b9
jimpak <jmpak1212@gmail.com>
2024-01-31 17:53:30 +0900

    upload

---
+++ bitc/PetPal/FirebaseApp/app/src/main/java/org/pjm/firebaseapp/AuthActivity.kt
@@ -0,0 +1,170 @@
+package org.pjm.firebaseapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.GoogleAuthProvider
+import org.pjm.firebaseapp.databinding.ActivityAuthBinding
+
+class AuthActivity : AppCompatActivity() {
+    lateinit var binding: ActivityAuthBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAuthBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (MyApplication.checkAuth()) {
+            changeVisibility("login")
+        } else {
+            changeVisibility("logout")
+        }
+
+        val requestLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(it.data)
+            try {
+                val account = task.getResult(ApiException::class.java)
+                val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+                MyApplication.auth.signInWithCredential(credential)
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            MyApplication.email = account.email // 구글이메일 인증서 저장
+                            changeVisibility("login")
+                        } else {
+                            changeVisibility("logout")
+                        }
+                    }
+            } catch (e: ApiException) {
+                changeVisibility("logout")
+            }
+        }
+
+        binding.googleLoginBtn.setOnClickListener {
+            val gso = GoogleSignInOptions
+                .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build()
+            val signInIntent = GoogleSignIn.getClient(this, gso).signInIntent
+            requestLauncher.launch(signInIntent)
+
+        }
+
+        binding.goSignInBtn.setOnClickListener {
+            changeVisibility("signin")
+        }
+
+        binding.signBtn.setOnClickListener {
+            //email,password 회원가입
+            val email = binding.authEmailEditView.text.toString()
+            val password = binding.authPasswordEditView.text.toString()
+
+            MyApplication.auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    binding.authEmailEditView.text.clear()
+                    binding.authPasswordEditView.text.clear()
+
+                    if (task.isSuccessful) {
+                        MyApplication.auth.currentUser?.sendEmailVerification()
+                            ?.addOnCompleteListener { sendTask ->
+                                if (sendTask.isSuccessful) {
+                                    Toast.makeText(
+                                        baseContext,
+                                        "회원가입 메일 유효성 검사 성공",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                    changeVisibility("logout")
+                                } else {
+                                    Toast.makeText(
+                                        baseContext,
+                                        "회원가입 메일 유효성 검사 실패",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                    changeVisibility("logout")
+                                }
+
+                            }
+                    } else {
+                        Toast.makeText(baseContext, "회원가입 실패", Toast.LENGTH_SHORT).show()
+                        changeVisibility("logout")
+                    }
+                }
+        }
+
+        binding.loginBtn.setOnClickListener {
+            //이메일, 비밀번호 로그인.......................
+            val email = binding.authEmailEditView.text.toString()
+            val password = binding.authPasswordEditView.text.toString()
+
+            MyApplication.auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    binding.authEmailEditView.text.clear()
+                    binding.authPasswordEditView.text.clear()
+
+                    if (task.isSuccessful) {
+                        if (MyApplication.checkAuth()) {
+                            MyApplication.email = email
+                            changeVisibility("login")
+                        } else {
+                            Toast.makeText(
+                                baseContext,
+                                "전송된 이메일로 인증처리가 되지 않았습니다.",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    } else {
+                        Toast.makeText(baseContext, "로그인 실패.", Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
+        binding.logoutBtn.setOnClickListener {
+            //로그아웃...........
+            MyApplication.auth.signOut()
+            MyApplication.email = null
+            changeVisibility("logout")
+        }
+    }
+
+    fun changeVisibility(mode: String) {
+        if (mode === "login") {
+            binding.run {
+                authMainTextView.text = "${MyApplication.email} 님 반갑습니다"
+                logoutBtn.visibility = View.VISIBLE
+                goSignInBtn.visibility = View.GONE
+                googleLoginBtn.visibility = View.GONE
+                authEmailEditView.visibility = View.GONE
+                authPasswordEditView.visibility = View.GONE
+                signBtn.visibility = View.GONE
+                loginBtn.visibility = View.GONE
+            }
+        } else if (mode === "logout") {
+            binding.run {
+                authMainTextView.text = "로그인 하거나 회원가입 해주세요."
+                logoutBtn.visibility = View.GONE
+                goSignInBtn.visibility = View.VISIBLE
+                googleLoginBtn.visibility = View.VISIBLE
+                authEmailEditView.visibility = View.VISIBLE
+                authPasswordEditView.visibility = View.VISIBLE
+                signBtn.visibility = View.GONE
+                loginBtn.visibility = View.VISIBLE
+            }
+        } else if (mode === "signin") {
+            binding.run {
+                logoutBtn.visibility = View.GONE
+                goSignInBtn.visibility = View.GONE
+                googleLoginBtn.visibility = View.GONE
+                authEmailEditView.visibility = View.VISIBLE
+                authPasswordEditView.visibility = View.VISIBLE
+                signBtn.visibility = View.VISIBLE
+                loginBtn.visibility = View.GONE
+            }
+        }
+    }
+}
